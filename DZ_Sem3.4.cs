d5703c9ebcc975b338e1f7e8d862666bab6f5fc7
Rick Solo <mrfox7398@mail.ru>
2022-08-31 19:50:48 +0300

    Zadacha 2

--- DZ_Sem3.4.cs
+++ DZ_Sem3.4.cs
@@ -21,9 +21,24 @@
 // --------------------------------------------------------------
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
 // 452 -> 11
-
 // 82 -> 10
-
 // 9012 -> 12
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum= "+ Metod(A));
+
+int Metod(int A){
+    int count = Convert.ToString(A).Length;
+    int temp = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        temp = A - A%10;
+        result = result + (A - temp);
+        A = A / 10;
+    }
+    return result;
+}
