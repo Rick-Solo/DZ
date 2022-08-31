503d1e0ce5bfbe5bbe9fd952e70682ad0b998564
Rick Solo <mrfox7398@mail.ru>
2022-08-31 18:37:55 +0300

    Popravka v Zadacha 1

--- DZ_Sem3.4.cs
+++ DZ_Sem3.4.cs
@@ -3,14 +3,27 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число В: ");
-int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(A + "^" + B + "=" + Stepen(A,B));
+// Console.Write("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число В: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(A + "^" + B + "=" + Stepen(A,B));
 
-double Stepen(int A, int B)
-{
-    double result = Math.Pow(A,B);
-    return result;
-}
+// double Stepen(int A, int B)
+// {
+//     double result = 1;
+//     int i = 0;
+//     while (i< Math.Abs(B))
+//         {result = result * A;
+//          i ++;}
+//     return result;
+// }
+// --------------------------------------------------------------
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
