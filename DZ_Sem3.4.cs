11c9274cdcf977a48632614136c713c63114b0e3
Rick Solo <mrfox7398@mail.ru>
2022-08-31 18:04:34 +0300

    Zadacha 1

--- DZ_Sem3.4.cs
+++ DZ_Sem3.4.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A + "^" + B + "=" + Stepen(A,B));
+
+double Stepen(int A, int B)
+{
+    double result = Math.Pow(A,B);
+    return result;
+}
