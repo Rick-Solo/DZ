0606cd74ed9510b1798aa17fd7c5bd18043b8f37
Rick Solo <mrfox7398@mail.ru>
2022-08-31 22:35:28 +0300

    Zadacha 3

--- DZ_Sem3.4.cs
+++ DZ_Sem3.4.cs
@@ -25,20 +25,50 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.Write("Введите число: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Sum= "+ Metod(A));
+// Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Sum= "+ Metod(A));
 
-int Metod(int A){
-    int count = Convert.ToString(A).Length;
-    int temp = 0;
-    int result = 0;
+// int Metod(int A){
+//     int count = Convert.ToString(A).Length;
+//     int temp = 0;
+//     int result = 0;
 
-    for (int i = 0; i < count; i++)
+//     for (int i = 0; i < count; i++)
+//     {
+//         temp = A - A%10;
+//         result = result + (A - temp);
+//         A = A / 10;
+//     }
+//     return result;
+// }
+// ---------------------------------------------------------------------------
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = NewArray (8);
+// Console.Write(string.Join(",",array));
+PrintArray(array);
+
+int [] NewArray (int size)
+{
+    int [] result = new int [size];
+    for (int i=0; i< size; i++ )
     {
-        temp = A - A%10;
-        result = result + (A - temp);
-        A = A / 10;
+        result [i] = new Random().Next();
     }
     return result;
+}
+
+void PrintArray(int[] mass)
+{
+    int count = mass.Length;
+    int index = 0;
+    while (index<count)
+    {
+        Console.Write(mass[index] + ",");
+        index++;
+    }
 }
